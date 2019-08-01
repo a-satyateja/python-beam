bdce18515d69158f78215801684a961bb0fc8c0d
a-satyateja <30695365+a-satyateja@users.noreply.github.com>
2019-08-01 20:52:52 +0530

    changes

---
+++ unpack.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import
+import argparse
+import logging
+
+
+import apache_beam as beam
+from apache_beam.io.filesystem import FileSystem
+from apache_beam.options.pipeline_options import PipelineOptions
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Specify default parameters.
+INPUT_FILE = 'gs://python-dataflow-example/data_files/image-list.txt'
+BQ_DATASET = 'ImageLabelFlow'
+BQ_TABLE = BQ_DATASET + '.dogs_short'
+
+
+def detect_labels_uri(uri):
+    """This Function detects labels in the image file located in Google Cloud Storage or on
+    theWeb and returns a comma separated list of labels. This will return an empty string
+    if not passed a valid image file
+    Args:
+        uri: a string link to a photo in gcs or on the web
+    (Adapted From:
+    https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/vision/cloud-client/detect/detect.py)
+    """
+    # Initialize cloud vision api client and image object.
+    client = vision.ImageAnnotatorClient()
+    image = types.Image()
+    image.source.image_uri = uri
+
+    # Send an api call for this image, extract label descriptions
+    # and return a comma-space separated string.
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    label_list = [l.description for l in labels]
+    return ', '.join(label_list)
+
+
+class ImageLabeler(beam.DoFn):
+    """
+    This DoFn wraps the inner function detect_labels_uri to write a BigQuery row dictionary
+    with the image file reference specified by the element from the prior PCollection and a list
+    of labels for that image assigned by the Google Cloud Vision API.
+    """
+
+    def process(self, element, *args, **kwargs):
+        """
+        Args:
+            element: A string specifying the uri of an image
+        Returns:
+            row: A list containing a dictionary defining a record to be written to BigQuery
+        """
+        labels_string = detect_labels_uri(element)
+
+        return [{'image_location': element, 'labels': labels_string}]
+
+
+def run():
+    p = beam.Pipeline(options=PipelineOptions())
+    input_pattern = 'abc'
+    (p
+     | 'Read from a File' >> FileSystem.match()
+     | 'Vision API label_annotation wrapper' >> beam.ParDo(ImageLabeler())
+     )
+    p.run().wait_until_finish()
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()

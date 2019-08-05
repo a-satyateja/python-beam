a49a74255f506ee367176822e83d0949dbeda17a
a-satyateja <30695365+a-satyateja@users.noreply.github.com>
2019-08-05 17:06:50 +0530

    changes

--- unpack.py
+++ unpack.py
@@ -1,8 +1,7 @@
 from __future__ import absolute_import
-# import argparse
 import logging
+from apache_beam.io import fileio
 import apache_beam as beam
-from apache_beam.io.gcp.gcsfilesystem import GCSFileSystem
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
@@ -33,7 +32,7 @@ from apache_beam.options.pipeline_options import PipelineOptions
 #     return ', '.join(label_list)
 
 
-class ImageLabeler(beam.DoFn):
+class ExtractFiles(beam.DoFn):
     """
     This DoFn wraps the inner function detect_labels_uri to write a BigQuery row dictionary
     with the image file reference specified by the element from the prior PCollection and a list
@@ -49,47 +48,45 @@ class ImageLabeler(beam.DoFn):
         Returns:
             row: A list containing a dictionary defining a record to be written to BigQuery
         """
-        print(element)
-        # print(element[0].path)
+
         # file_path = element[0].path
         logging.info(repr(element))
-        for el in element:
-            with gcsio.GcsIO().open(el.path, 'r') as f:
-                if zipfile.is_zipfile(f):
-                    logging.info('it is a zip file')
-                    z = zipfile.ZipFile(f)
-                    file_list = z.filelist
-                    # print file_list[0].filename
-                    for some_file in file_list:
-                        if '.TIF' in some_file.filename:
-                            print some_file.filename
-                            logging.info('zip file is : ', some_file.filename)
-                            try:
-                                data = z.read(some_file)
-                            except KeyError:
-                                print 'ERROR: Did not find %s in zip file' % some_file
-                            else:
-                                print some_file.filename, ':'
-                                outfile = 'gs://dataflow-buffer/python-1/' + some_file.filename
-                                with gcsio.GcsIO().open(outfile, mode='w',
-                                                        mime_type='image/tiff') as writing_path:
-                                    writing_path.write(data)
-                                    writing_path.close()
-                                    print 'write success'
+        # print(element[0].path)
+        # print(len(element))
+        # for el in element:
+        with gcsio.GcsIO().open(element.path, 'r') as f:
+            if zipfile.is_zipfile(f):
+                logging.info('it is a zip file')
+                z = zipfile.ZipFile(f)
+                file_list = z.filelist
+                # print file_list[0].filename
+                for some_file in file_list:
+                    if '.TIF' in some_file.filename:
+                        print some_file.filename
+                        logging.info('zip file is : %s', some_file.filename)
+                        try:
+                            data = z.read(some_file)
+                        except KeyError:
+                            print 'ERROR: Did not find %s in zip file' % some_file
+                        else:
+                            print some_file.filename, ':'
+                            outfile = 'gs://dataflow-buffer/python-2/' + some_file.filename
+                            with gcsio.GcsIO().open(outfile, mode='w',
+                                                    mime_type='image/tiff') as writing_path:
+                                writing_path.write(data)
+                                writing_path.close()
+                                print 'write success'
         return 'done'
 
 
 def run():
     p = beam.Pipeline(options=PipelineOptions())
-    gcs = GCSFileSystem(PipelineOptions())
     # 'gs://dataflow-buffer/parent-unpack/2018/i20180130/PxpFJwJabD-untarI20180130/DESIGN/USD0808610-20180130.ZIP'
-    input_pattern = [
-        'gs://dataflow-buffer/parent-unpack/2018/i20180130/PxpFJwJabD-untar*/**/*.ZIP']
+    input_pattern = 'gs://dataflow-buffer/parent-unpack/2018/i20180130/PxpFJwJabD-untar*/**/*.ZIP'
 
-    result = [m.metadata_list for m in gcs.match(input_pattern)]
     (p
-     | 'Read from a File' >> beam.Create(result)
-     | 'Print read file' >> beam.ParDo(ImageLabeler())
+     | 'Match Files' >> fileio.MatchFiles(input_pattern)
+     | 'Extract Files' >> beam.ParDo(ExtractFiles())
      )
     p.run().wait_until_finish()
 
